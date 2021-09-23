0e635861839171f62c694d6dac26c65025dcac9f
Michael Doronin <m.doronin@kryptonite.ru>
2021-09-23 19:18:51 +0300

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,57 @@ use std::{
     collections::VecDeque,
     env,
     fs::{self, DirEntry},
-    io, iter,
+    io::{self, Write},
+    iter,
+    os::unix::prelude::OsStrExt,
     path::{Path, PathBuf},
 };
 
-fn recurse_dir<P: AsRef<Path>>(dir: P) -> impl Iterator<Item = io::Result<PathBuf>> {
+fn recurse_dir<P: AsRef<Path>>(dir: P) -> impl Iterator<Item = io::Result<DirEntry>> {
     let mut unvisited = VecDeque::new();
-    unvisited.push_back(dir.as_ref().to_path_buf());
-    let mut entries: Box<dyn Iterator<Item = io::Result<DirEntry>>> = Box::new(iter::empty());
+    // unvisited.push_back(dir.as_ref().to_path_buf());
+    let mut maybe_entries = fs::read_dir(dir);
     iter::from_fn(move || loop {
-        match entries.next() {
-            Some(Ok(entry)) => match entry.file_type() {
-                Ok(filetype) => {
-                    let path = entry.path();
-                    if filetype.is_dir() {
-                        unvisited.push_back(path)
-                    } else if filetype.is_file() {
-                        return Some(Ok(path));
-                    }
+        match &mut maybe_entries {
+            Ok(entries) => {
+                match entries.next() {
+                    Some(Ok(entry)) => {
+                        match entry.file_type() {
+                            Ok(filetype) => {
+                                if filetype.is_dir() {
+                                    unvisited.push_back(entry.path())
+                                }
+                                return Some(Ok(entry));
+                            },
+                            Err(e) => return Some(Err(e)),
+                        }
+                    },
+                    Some(Err(e)) => return Some(Err(e)),
+                    None => {
+                        let dir = unvisited.pop_front()?;
+                        maybe_entries = fs::read_dir(dir);
+                    },
                 }
-                Err(err) => return Some(Err(err)),
             },
-            Some(Err(err)) => return Some(Err(err)),
-            None => {
-                let dir = unvisited.pop_front()?;
-                match fs::read_dir(dir) {
-                    Ok(new_entries) => {
-                        entries = Box::new(new_entries);
-                    }
-                    Err(err) => return Some(Err(err)),
-                }
-            }
-        }
+            Err(_) => return Some(Err(io::Error::last_os_error())),
+        };
     })
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut args = env::args().skip(1);
     let dir = args.next().unwrap_or(".".to_string());
+    let mut out = io::stdout();
+    let mut err = io::stderr();
     for entry in recurse_dir(dir) {
         match entry {
-            Ok(path) => {
-                println!("{}", path.to_string_lossy())
+            Ok(entry) => {
+                out.write_all(entry.path().as_os_str().as_bytes())?;
             }
             Err(e) => {
-                eprintln!("{}", e)
+                write!(err, "{}", e)?;
             }
         }
     }
+    Ok(())
 }
